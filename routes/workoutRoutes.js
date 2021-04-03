6941a9dc8bec32ad59f40ba414f941b511de323a
Sean Nguyen <nsean321@gmail.com>
2021-04-02 21:20:43 -0700

    test

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,12 +1,6 @@
 const router = require('express').Router()
 const { Workout } = require('../models')
 
-// get a workout by id
-router.get('/workout/:id', (req, res) => {
-  Workout.findById()
-    .then(workout => res.json(workout))
-    .catch(err => console.log(err))
-})
 // get all workouts
 router.get('/workouts', (req, res) => {
   Workout.find()
@@ -17,6 +11,12 @@ router.get('/workouts', (req, res) => {
 router.get('/workouts/range', (req, res) => {
   Workout.find(req.body).limit(7)
     .then(workouts => res.json(workouts))
+    .catch(err => console.log(err))
+})
+// get a workout by id
+router.get('/workout/:id', (req, res) => {
+  Workout.findById()
+    .then(workout => res.json(workout))
     .catch(err => console.log(err))
 })
 // create a workout
