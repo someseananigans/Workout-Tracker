5bfd0e14dcb212a003138c6e12c8dbeabe813354
Sean Nguyen <nsean321@gmail.com>
2021-04-05 22:34:23 -0700

    server for heroku

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use(require('./routes'))
 
 require('./db')
-  .then(() => app.listen(PORT))
+  .then(() => app.listen(process.env.PORT || PORT))
   .catch(err => console.log(err))
