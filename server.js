9357c2b570392ee319921b7504f74fbd844437c2
Sean Nguyen <nsean321@gmail.com>
2021-04-05 23:04:54 -0700

    debug

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { join } = require('path')
 
-const PORT = 3000
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
@@ -11,5 +10,5 @@ app.use(express.json())
 app.use(require('./routes'))
 
 require('./db')
-  .then(() => app.listen(process.env.PORT || PORT))
+  .then(() => app.listen(process.env.PORT || 3000))
   .catch(err => console.log(err))
